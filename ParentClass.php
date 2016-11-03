16a5e120ea2ea4937e925bb3697a512a3927f03c
Nicole Daniels <nzoner12@yahoo.com>
2016-11-03 12:22:41 -0400

    Create parent class with pig latin translations

--- ParentClass.php
+++ ParentClass.php
@@ -2,5 +2,37 @@
 	// This is the file for the parent class
 
 	class ParentClass {
-		
+
+    public $regularString;
+
+    public function __construct($regularString){
+      $this->regularString = $regularString;  
+    }
+
+    public function getRegularString(){
+      return $this->regularString;
+    }
+
+    //Translate the given string into pig latin
+    public function toPigLatin($originalString){
+      $pigEnding = "ay";
+      $lowercaseInput = strtolower($input);
+      $firstLetter = $lowercaseInput[0];
+
+      if ($firstLetter == "a" || $firstLetter == "e" || $firstLetter == "i" || $firstLetter == "o" || $firstLetter == "u"){
+        $pigWord = "$lowercaseInput$pigEnding";
+      }
+      else {
+        $pigWord = substr($lowercaseInput, 1) . "" . "$firstLetter$pigEnding";
+      }
+      return $pigWord;
+    }
+
+    //Return the result of the pig latin translation
+    public function __toString(){
+      $pigLatinWord = toPigLatin($this->regularString);
+      return $pigLatinWord;
+    }
+
 	}
+?>
